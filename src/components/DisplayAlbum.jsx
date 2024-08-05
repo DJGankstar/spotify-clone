ac16824dbe3d7b544ca91348a2fafeb8c4b4a6c9
DJGankstar <117345527+DJGankstar@users.noreply.github.com>
2024-08-05 17:39:11 +1000

    Refactor Display component and add background color to album view

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -12,14 +12,14 @@ export const DisplayAlbum = () => {
   return (
     <>
         <Navbar/>
-        <div className = "gt-10 flex gap-8 flex-col md:flex-row md:items-end">
+        <div className = "mt-10 flex gap-8 flex-col md:flex-row md:items-end">
             <img className = "w-48 rounded" src={albumData.image} alt=""/>
             <div className = "flex flex-col">
                 <p>Playlist</p>
                 <h2 className = "text-5xl font-bold mb-4 md:text-7xl">{albumData.name}</h2>
                 <h4>{albumData.desc}</h4>
                 <p className = "mt-1">
-                    <img className = "inline-block w-5" src = {assets.spotify_logo} alt=""/>
+                    <img className = "inline-block w-5" src = {assets.spotify_logo} alt="Spotify Logo"/>
                     <b>Spotify</b>
                     • 1,323,154 like
                     • <b>50 Songs, </b>
@@ -30,9 +30,9 @@ export const DisplayAlbum = () => {
        <div className = "grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
         <p>
             <b className = "mr-3">#</b>Title</p>
-            <p>ALbum</p>
+            <p>Album</p>
             <p className = "hidden sm:block">Date Added</p>
-            <img className = "m-auto w-4" src = {assets.clock_icon} alt=""/>
+            <img className = "m-auto w-4" src = {assets.clock_icon} alt="Clock Icon"/>
        </div>
        <hr/>
        {
@@ -40,7 +40,7 @@ export const DisplayAlbum = () => {
             <div key = {index} className = "grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer">
                 <p className = "text-white">
                     <b className = "mr-4 text-[#a7a7a7]">{index + 1}</b>
-                    <img className = "inline w-10 mr-5" src = {item.image} alt=""/>
+                    <img className = "inline w-10 mr-5" src = {item.image} alt={item.name}/>
                     {item.name}
                 </p>
                 <p className = "text-[15px]">{albumData.name}</p>
